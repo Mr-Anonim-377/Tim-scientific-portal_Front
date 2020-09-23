ef53465e562d9864dd6e17bc306e1da6d01e13eb
Alexander Fedotov <sashafdtv@gmail.com>
2020-09-23 19:05:19 +0300

    [#3] - Create frontend

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="https://www.timacad.ru/uploads/media/cache/image_sm_resize/uploads/images/20171115/1510754611_emblema.jpg">
+  <HelloWorld></HelloWorld>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 200px;
+}
+</style>
