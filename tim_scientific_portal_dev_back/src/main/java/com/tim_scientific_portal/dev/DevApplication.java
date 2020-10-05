2125dae4244b21effe65cbad667833bc9397489b
MrAnonim-377 <w.zzzz@mail.ru>
2020-10-05 04:21:43 +0300

    [#5] - create back project and restructure all projects

---
+++ tim_scientific_portal_dev_back/src/main/java/com/tim_scientific_portal/dev/DevApplication.java
@@ -0,0 +1,13 @@
+package com.tim_scientific_portal.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevApplication.class, args);
+	}
+
+}
