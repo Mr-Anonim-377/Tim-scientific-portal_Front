63c5c438d8ea0c6b9cb5d940f15b2018344baf41
Влад <wladisalw31011998@mail.ru>
2020-10-30 03:34:15 +0300

    Removed the extra classes and moved them to the components of the program direction.

--- src/components/routComponents/MainPage.vue
+++ src/components/routComponents/MainPage.vue
@@ -1,24 +1,17 @@
 <template>
   <Header :modifiers="modifiers.header"/>
   <router-view/>
-  <!-- <MainPageBody/> -->
-<!--  <ProgramsPageBody/>-->
   <Footer/>
 </template>
 
 <script>
 import Header from "../unitComponents/pattern/header/Header";
-// import MainPageBody from "../unitComponents/body/controls/MainPageBody";
 import Footer from "../unitComponents/pattern/footer/Footer";
-import MainPageBody from "../unitComponents/body/controls/MainPageBody";
-// import ProgramsPageBody from "../unitComponents/body/controls/ProgramsPageBody";
 
 export default {
   name: "MainPage",
   components: {
     Header,
-    // MainPageBody,
-    // ProgramsPageBody,
     Footer
   },
 
