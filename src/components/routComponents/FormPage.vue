47d6d43bf6311b650ae6bab91bbbe259d6783205
afedotov <afedotov@team.amocrm.com>
2021-02-19 02:46:31 +0300

    правки по анкете

--- src/components/routComponents/FormPage.vue
+++ src/components/routComponents/FormPage.vue
@@ -4,43 +4,54 @@
 			<TitleSection :title="title" :headerVisible="true" :stileTitle="stileTitle.stile" />
 
 			<div class="form-container">
-				<account-navigation-section />
+				<div class="form-nav">
+					<account-navigation-section />
+				</div>
 
-				<img :src="this.PERSONAL_INFORMATION.IMAGE[0].image" />
+				<div class="form-content">
+					<div class="form-content-header">
+						<div class="form-content-image">
+							<img :src="this.PERSONAL_INFORMATION.IMAGE[0].image" />
+						</div>
+						<div class="form-block__information">
+							<h2>{{ PERSONAL_INFORMATION.PERSONAL_INFORMATION[0].text }}</h2>
+							<p class="information-text">32 года / Профессор ботаники</p>
 
-				<div class="form-block__information">
-					<h2>Вострухин Игорь Александрович</h2>
-					<p class="information-text">32 года / Профессор ботаники</p>
-					<p class="form-block__text-project">
-						Участие в проектах:
-					</p>
-					<div class="project-container">
-						<div class="project-container-block">
-							<router-link style="text-decoration: none" :to="{ name: 'notFound' }">
-								<img :src="1" />
-								<p>Проект</p>
-							</router-link>
+							<div class="form-block__edit">
+								<h3>Ф.И.О.</h3>
+								<input type="text" placeholder="Как Вас зовут..." class="edit__inp" v-if="authorization" />
+
+								<p class="edit-text" v-else>{{ PERSONAL_INFORMATION.PERSONAL_INFORMATION[0].text }}</p>
+
+								<h3>Дата рождения</h3>
+								<input type="text" placeholder="Выбрать дату" class="edit__inp" v-if="authorization" />
+								<p class="edit-text" v-else>{{ PERSONAL_INFORMATION.PERSONAL_INFORMATION[0].date }}</p>
+
+								<h3>Специальность</h3>
+								<input type="text" placeholder="Выбрать специальность" class="edit__inp" v-if="authorization" />
+								<p class="edit-text" v-else>{{ PERSONAL_INFORMATION.RESEARCH_EDUCATION[0].title }}</p>
+
+								<h3>Образование</h3>
+								<input type="text" placeholder="Выбрать участника консорциума" class="edit__inp" v-if="authorization" />
+								<p class="edit-text" v-else>{{ PERSONAL_INFORMATION.RESEARCH_EDUCATION[0].text }}</p>
+							</div>
+
+							<!-- Пока решил убрать, чушь какая-то -->
+
+							<!-- <p class="form-block__text-project">
+								Участие в проектах:
+							</p>
+							<div class="project-container">
+								<div class="project-container-block">
+									<router-link style="text-decoration: none" :to="{ name: 'notFound' }">
+										<img :src="1" />
+										<p>Проект</p>
+									</router-link>
+								</div>
+							</div> -->
 						</div>
 					</div>
 				</div>
-			</div>
-
-			<div class="form-block__edit">
-				<h3>Ф.И.О.</h3>
-				<input type="text" placeholder="Как Вас зовут..." class="edit__inp" v-if="authorization" />
-				<p class="edit-text" v-else>е</p>
-
-				<h3>Дата рождения</h3>
-				<input type="text" placeholder="Выбрать дату" class="edit__inp" v-if="authorization" />
-				<p class="edit-text" v-else>31.01.1989</p>
-
-				<h3>Специальность</h3>
-				<input type="text" placeholder="Выбрать специальность" class="edit__inp" v-if="authorization" />
-				<p class="edit-text" v-else>Профессор ботаники</p>
-
-				<h3>Образование</h3>
-				<input type="text" placeholder="Выбрать участника консорциума" class="edit__inp" v-if="authorization" />
-				<p class="edit-text" v-else>РГАУ МСХА им.Тимирязева</p>
 			</div>
 		</section>
 	</div>
@@ -98,6 +109,7 @@
 			 * Берем PERSONAL_INFORMATION
 			 */
 			console.log(this.PERSONAL_INFORMATION);
+			console.log(this.PERSONAL_INFORMATION.PERSONAL_EDUCATION);
 		},
 	};
 </script>
@@ -126,27 +138,53 @@
 	.form-container {
 		display: flex;
 		margin-top: 94px;
+		max-width: 1200px;
+		margin: 50px auto;
+		justify-content: space-around;
+	}
+
+	.form-content-header {
+		/* margin-top: 40px; */
+		display: flex;
+		align-items: flex-start;
+		justify-content: flex-start;
+		/* margin: 0; */
+	}
+
+	.form-content-image {
+		width: 220px;
+		height: 220px;
+		border-radius: 50%;
+		border: 3px solid #3f7e77;
+		overflow: hidden;
+		display: flex;
+		align-items: center;
+		justify-content: center;
 	}
 
 	img {
-		width: 220px;
-		height: 220px;
+		/* width: 220px; */
+		/* height: 220px; */
 		background: #c4c4c4;
 		background-size: contain;
-		margin: 0 60px 0 130px;
-		border-radius: 50%;
-		border: 3px solid #3f7e77;
+		/* margin: 0 60px 0 130px; */
 	}
 
 	.form-block__information {
+		width: 550px;
 		display: flex;
 		flex-wrap: wrap;
+		text-align: center;
+		/* justify-content: center; */
+		/* margin: auto; */
 	}
 
 	.information-text {
 		position: relative;
-		margin: 0 0 4px 47px;
+		margin: 0 auto 4px;
+		width: 100%;
 	}
+
 	.information-text:before {
 		content: '';
 		position: absolute;
@@ -220,23 +258,30 @@
 
 	.form-block__edit {
 		margin: 49px auto 0 auto;
-		width: 762px;
+		/* width: 762px; */
+		width: 60%;
 	}
 	.form-block__edit h3 {
 		font-weight: bold;
 		font-size: 15px;
 		line-height: 18px;
-		color: #3f7e77;
+		background-color: #3f7e77;
 		position: relative;
+
+		padding: 10px;
+		color: white;
+		border-radius: 20px;
+		/* width: 220px; */
+		text-align: center;
 	}
-	.form-block__edit h3:before {
-		content: '';
+	/* .form-block__edit h3:before { */
+	/* content: '';
 		position: absolute;
 		width: 35px;
 		height: 1px;
 		background: #3f7e77;
-		top: -16px;
-	}
+		top: -16px; */
+	/* } */
 
 	.edit__inp {
 		width: 416px;
@@ -265,6 +310,8 @@
 
 	.edit-text {
 		margin: 20px 0 30px 5px;
+		text-align: center;
+		/* width: 220px; */
 		font-weight: bold;
 		font-size: 16px;
 		line-height: 19px;
