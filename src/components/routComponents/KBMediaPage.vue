fa3e078b0aeb55ac9aa4a2a3c9a6196dedc8b7e4
afedotov <afedotov@team.amocrm.com>
2021-04-09 02:22:41 +0300

    БЗ

---
+++ src/components/routComponents/KBMediaPage.vue
@@ -0,0 +1,68 @@
+<template>
+    <section v-if="loadSuccess">
+        <TitleSection :title="this.VIDEOS.VIDEO[0].title" :headerVisible="true" />
+        <iframe
+            width="1140"
+            height="642"
+            :src="this.VIDEOS.VIDEO[0].link"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+        ></iframe>
+    </section>
+    <div v-else>
+        <Preloader />
+    </div>
+</template>
+
+<script>
+    import Preloader from './../unitComponents/CommonElements/Preloader';
+    import testMixin from '../../utils/methodsMixin.js';
+
+    import TitleSection from '../unitComponents/TitleSection';
+
+    export default {
+        mixins: [testMixin],
+        props: {
+            pageId: String,
+        },
+        name: 'KBMediaPage',
+        components: {
+            TitleSection,
+            Preloader,
+        },
+        data() {
+            return {
+                loadSuccess: false,
+            };
+        },
+        async mounted() {
+            await this.getModulesTest('', this.pageId);
+            setTimeout(() => {
+                console.log(this.VIDEOS);
+                this.loadSuccess = true;
+            }, 500);
+        },
+    };
+</script>
+
+<style scoped>
+    iframe {
+        margin-top: 50px;
+    }
+    section {
+        color: #3f7e77;
+        max-width: 1140px;
+        align-items: center;
+        margin: 100px auto 170px auto;
+        min-height: calc(100vh - 1010px);
+    }
+
+    section p {
+        font-size: 15px;
+        line-height: 150%;
+        color: #3f7e77;
+        margin-bottom: 20px;
+    }
+</style>
