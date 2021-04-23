75338eec2cd88fdd38b1efa18dc08c318012ce40
afedotov <afedotov@team.amocrm.com>
2021-04-23 15:38:21 +0300

    Работа с формой - загрузка изображений

--- src/components/routComponents/UI_test.vue
+++ src/components/routComponents/UI_test.vue
@@ -1,13 +1,14 @@
 <template>
-    <!-- test -->
+    <!-- Форма добавления/редактирования новости -->
+    <!-- UI: https://element-plus.org/ -->
     <section>
         <TitleSection title="Форма редактирования сущности" :headerVisible="true" />
 
         <div class="form">
             <!-- Grid разметка -->
-            <el-row :gutter="20" type="flex" justify="space-between">
-                <el-col :span="10">
-                    <!-- Название новости -->
+
+            <el-row type="flex" justify="center">
+                <el-col>
                     <h1>Название новости</h1>
                     <el-input
                         type="textarea"
@@ -19,24 +20,56 @@
                         placeholder="Название"
                     ></el-input>
                 </el-col>
-                <el-col :span="10">
-                    <h1>Изображения</h1>
+            </el-row>
+            <el-row :gutter="20" type="flex" justify="space-between">
+                <!-- Название новости -->
+                <el-col :span="12">
+                    <!-- Изображения -->
+                    <h1>Изображения для слайдера</h1>
+                    <!-- Временно попробуем грузить через API IMGbb -->
+                    <!-- https://api.imgbb.com/ -->
+                    <el-upload
+                        class="upload-demo"
+                        action="https://api.imgbb.com/1/upload"
+                        multiple
+                        :data="requestData"
+                        list-type="picture"
+                        :limit="3"
+                        :file-list="sliderFileList"
+                        name="image"
+                    >
+                        <el-button size="small" type="primary">Выберите изображения</el-button>
+                        <template #tip>
+                            <div class="el-upload__tip">jpg/png файлы размером не более 500кб</div>
+                        </template>
+                    </el-upload>
+                </el-col>
+                <el-col :span="12">
+                    <!-- Изображения -->
+                    <h1>Изображения для превью</h1>
+                    <el-upload
+                        class="upload-demo"
+                        action="https://api.imgbb.com/1/upload"
+                        multiple
+                        :data="requestData"
+                        list-type="picture"
+                        :limit="1"
+                        :file-list="previewfileList"
+                        name="image"
+                    >
+                        <el-button size="small" type="primary">Выберите изображение</el-button>
+                        <template #tip>
+                            <div class="el-upload__tip">jpg/png файл размером не более 500кб</div>
+                        </template>
+                    </el-upload>
                 </el-col>
             </el-row>
         </div>
 
         <div class="form">
-            <!-- Дата -->
-
-            <!-- <el-date-picker v-model="date" type="date" placeholder="Pick a day"> </el-date-picker> -->
-
-            <!-- Изображения -->
-
             <!-- Текст -->
             <h1>Тело новости</h1>
             <el-input @input="clg" type="textarea" :rows="5" placeholder="Тело новости" v-model="text" maxlength="200" show-word-limit> </el-input>
-
-            <!-- Превью -->
         </div>
     </section>
 </template>
@@ -58,9 +91,21 @@
 
         data() {
             return {
+                /* Название новости */
                 title: '',
+
+                /* Дата загрузки новости */
                 date: '',
+
+                /* Текст новости */
                 text: '',
+
+                /*  При загрузке изображений отправляем api key imgBB
+                    Если что-то пойдет не так, получить новый можно тут:
+                    https://api.imgbb.com/ */
+                requestData: {
+                    key: '2ca9c35e0d42896ec7e746b5daf2c924',
+                },
             };
         },
     };
@@ -68,8 +113,8 @@
 
 <style scoped>
     section {
-        width: 800px;
-        margin: 100px auto;
+        width: 600px;
+        margin: 50px auto;
         min-height: calc(100vh - 275px);
     }
 
@@ -77,5 +122,13 @@
         color: #3f7e77;
         text-align: center;
         font-size: 20px;
+    }
+
+    .upload-demo {
+        text-align: center;
+    }
+
+    .el-row {
+        margin: 40px 0;
     }
 </style>
