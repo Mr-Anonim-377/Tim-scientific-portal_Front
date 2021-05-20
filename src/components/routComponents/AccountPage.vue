d440944beadc9aca67a355f9cbc7f4fc2a054225
afedotov <afedotov@team.amocrm.com>
2021-05-21 01:56:23 +0300

    Правки по достижениям

--- src/components/routComponents/AccountPage.vue
+++ src/components/routComponents/AccountPage.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="account" v-if="authorization">
+    <section class="account">
         <TitleSection :title="titleAccount" :headerVisible="true" :stileTitle="stileTitle.stile" />
 
         <div class="account-container">
@@ -60,6 +60,12 @@
              * Получаем данные по id
              */
             await this.getModulesTest('', this.profileID);
+
+            //eslint-disable-next-line
+            const self = this;
+            //eslint-disable-next-line
+            debugger;
+
             this.loadSuccess = true;
 
             /**
